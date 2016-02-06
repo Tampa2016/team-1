9caad66c8bc00721cec0548bf2ea3b08bc3b74d0
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2016-02-05 22:48:23 -0500

    Fixed index.php

--- index.php
+++ index.php
@@ -1,5 +1,43 @@
+<<<<<<< HEAD
+<?php include "_include/classes.php"; include "_include/theme/head.php";  ?>
+  
+    <!-- Intro Section -->
+    <section id="intro" class="intro-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>"ESTABLISH BETTER QUALITY OF LIFE FOR PEOPLE WITH DISABILITIES."</h1>
+                    <a class="btn btn-default page-scroll" href="#info">Down</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Info Section -->
+    <section id="info" class="info-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Info</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Map Section -->
+    <section id="map" class="map-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Map</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+=======
 <?php include "_include/classes.php"; include "_include/theme/head.php";   ?>
     <h1>Code for Good!</h1>
+>>>>>>> d6c7288cd225efb4e48a895af196a409e7f573d9
 
 
 
