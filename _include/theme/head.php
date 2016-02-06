a054f71ba0b2dff33b61b5896e22a0574351cad7
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2016-02-06 05:32:54 -0500

    Fixed head and finallized title

--- _include/theme/head.php
+++ _include/theme/head.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Reeve Foundation</title>
+    <title>Evermore</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
--- index.php
+++ index.php
@@ -83,35 +83,35 @@
 				<div class="responsive">
 					<div>
 						<div class="review">
-							<h3>Title <a style="text-align:right" href="https://twitter.com/share" class="twitter-share-button">Tweet</a></h3>
+							<h3>Title </h3>
 							<p>Content</p>
 							<p class="posted"> 10:00 @ 2/6/2016</p>
 						</div>
 					</div>
 					<div>
 						<div class="review">
-							<h3>Title <a style="text-align:right" href="https://twitter.com/share" class="twitter-share-button">Tweet</a></h3>
+							<h3>Title </h3>
 							<p>Content</p>
 							<p class="posted"> 10:00 @ 2/6/2016</p>
 						</div>
 					</div>
 					<div>
 						<div class="review">
-							<h3>Title <a style="text-align:right" href="https://twitter.com/share" class="twitter-share-button">Tweet</a></h3>
+							<h3>Title </h3>
 							<p>Content</p>
 							<p class="posted"> 10:00 @ 2/6/2016</p>
 						</div>
 					</div>
 					<div>
 						<div class="review">
-							<h3>Title <a style="text-align:right" href="https://twitter.com/share" class="twitter-share-button">Tweet</a></h3>
+							<h3>Title </h3>
 							<p>Content</p>
 							<p class="posted"> 10:00 @ 2/6/2016</p>
 						</div>
 					</div>
 					<div>
 						<div class="review">
-							<h3>Title <a style="text-align:right" href="https://twitter.com/share" class="twitter-share-button">Tweet</a></h3>
+							<h3>Title </h3>
 							<p>Content</p>
 							<p class="posted"> 10:00 @ 2/6/2016</p>
 						</div>
