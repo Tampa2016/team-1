aa2bd1440fc8c8c43b5ae883057a69b91a424781
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2016-02-05 19:49:16 -0500

    Picture table and user table modifications

--- SQL/picture_table.sql
+++ SQL/picture_table.sql
@@ -1,3 +1,4 @@
+/*user_pk == column pk in table users */
 create table pictures (
 picture_pk varchar(25) not null,
 picture blob not null,
@@ -6,4 +7,5 @@ picture_type varchar(25) not null default '',
 picture_name varchar(100) not null default '',
 user_pk varchar(25) not null,
 picture_info varchar(256) not null default '',
-)
+PRIMARY KEY picture_pk
+);
---
+++ SQL/user_table.sql
@@ -0,0 +1,6 @@
+create table user (
+pk varchar (25) not null,
+badges varchar (25) not null default '',
+points varchar (25) not null,
+PRIMARY KEY pk
+);
